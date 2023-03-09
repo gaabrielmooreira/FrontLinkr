a2ea38cb01a58580bb41bca8c9f20df85484bb56
kendy-karakawa <kendy.karakawa@gmail.com>
2023-03-08 22:28:06 -0300

    feat: signout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import { HeaderContainer, InputDiv, InputSearch, LogoutContainer, PerfilContainer, StyledH1, UserImg } from "./Styled";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AiOutlineSearch } from 'react-icons/ai';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../context/auth";
+import apiAuth from "../../services/apiAuth";
 
 export default function Header(props) {
     const { withSearch } = props;
     const [showLogout, setShowLogout] = useState(false);
+    const {userAuth} = useContext(AuthContext)
     const navigate = useNavigate();
 
     const closeMenu = () => showLogout && setShowLogout(false);
@@ -17,6 +20,8 @@ export default function Header(props) {
     }
 
     const logout = () => {
+        apiAuth.signOut(userAuth.token)
+        localStorage.removeItem("user")
         navigate('/sign-in');
         closeMenu();
     }
@@ -34,7 +39,7 @@ export default function Header(props) {
 
             <PerfilContainer onClick={() => setShowLogout(!showLogout)}>
                 {showLogout ? <IoIosArrowUp size="30px" color="#FFF"/>:<IoIosArrowDown size="30px" color="#FFF"/>}
-                <UserImg alt="perfil-image" />
+                <UserImg src={userAuth.url} alt="perfil-image" />
             </PerfilContainer>
 
             <LogoutContainer showLogout={showLogout}>
