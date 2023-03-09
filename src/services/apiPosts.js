7e9e950ecd9135e307127c04b2bf01e94628909c
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-03-09 01:36:35 -0300

    feat: services apiPosts

--- src/services/apiPosts.js
+++ src/services/apiPosts.js
@@ -6,7 +6,27 @@ async function getPostsByHashtag(hashtag,token) {
     return res
 }
 
+async function getPosts(token){
+    const {data: res} = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/posts`, createConfig(token)); 
+    return res;
+}
+
+async function toggleLike(idPost,token){
+    await axios.post(`${process.env.REACT_APP_API_BASE_URL}/posts/${idPost}/like`, createConfig(token));
+}
+async function updatePost(idPost, token, body){
+    await axios.put(`${process.env.REACT_APP_API_BASE_URL}/posts/${idPost}`, body, createConfig(token));
+}
+
+async function deletePost(idPost, token){
+    await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/posts/${idPost}`, createConfig(token));
+}
+
 const apiPosts = {
     getPostsByHashtag,
+    deletePost,
+    updatePost,
+    getPosts,
+    toggleLike,
 }
 export default apiPosts;
