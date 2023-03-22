4179078b8acef4574516a1a720e83a58daa6a65d
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-03-21 21:19:10 -0300

    feat: refreshNewPosts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,21 @@ import PostsMainSection from "../components/PostsMainSection/PostsMainSection";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.js";
 import apiPosts from "../services/apiPosts.js";
+import useInterval from "use-interval";
 
 export default function HomePage() {
     const [posts, setPosts] = useState([]);
+    const [newPostsAvailable, setNewPostsAvailable] = useState(0);
     const [postsAreChanged, setPostsAreChanged] = useState(false);
     const { userAuth } = useContext(AuthContext);
+    const [dateOfLastUpdate, setDateOfLastUpdate] = useState('');
 
     useEffect(() => {
         async function getData() {
-            try{
+            try {
                 const data = await apiPosts.getPosts(userAuth.token);
                 setPosts(data);
+                setDateOfLastUpdate(Date.now());
             } catch {
                 alert('An error occured while trying to fetch the posts, please refresh the page');
             }
@@ -20,8 +24,38 @@ export default function HomePage() {
         getData();
     }, [postsAreChanged])
 
+    useInterval(async () => {
+        try {
+            const data = await apiPosts.getPostsAfterDate(dateOfLastUpdate, userAuth.token);
+            const newPostsLength = data.length;
+            console.log(newPostsLength);
+            setNewPostsAvailable(newPostsLength);
+        } catch (err) {
+            console.log(err);
+        }
+    }, 15000);
+
+    const getNewPosts = async () => {
+        try {
+            const data = await apiPosts.getPostsAfterDate(dateOfLastUpdate, userAuth.token);
+            const arrNewPosts = [...data, ...posts];
+            setPosts(arrNewPosts);
+            setDateOfLastUpdate(Date.now());
+            setNewPostsAvailable(0);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <PostsMainSection title={'timeline'} posts={!posts? 'carregando' : posts} postsAreChanged={postsAreChanged} setPostsAreChanged={setPostsAreChanged}>
+        <PostsMainSection 
+            title={'timeline'} 
+            posts={!posts ? 'carregando' : posts} 
+            postsAreChanged={postsAreChanged} 
+            setPostsAreChanged={setPostsAreChanged}
+            newPostsAvailable={newPostsAvailable}
+            getNewPosts={getNewPosts}
+        >
         </PostsMainSection>
     )
 }
--- src/services/apiPosts.js
+++ src/services/apiPosts.js
@@ -1,36 +1,41 @@
 import axios from "axios";
 import { createConfig } from "./apiAuth";
 
-async function getPostsByHashtag(hashtag,token) {
-    const {data: res} = await axios.get(`${process.env.REACT_APP_API_URL}/hashtag/${hashtag}`, createConfig(token)); 
+async function getPostsByHashtag(hashtag, token) {
+    const { data: res } = await axios.get(`${process.env.REACT_APP_API_URL}/hashtag/${hashtag}`, createConfig(token));
     return res
 }
 
-async function getPosts(token){
-    const {data: res} = await axios.get(`${process.env.REACT_APP_API_URL}/posts`, createConfig(token)); 
+async function getPosts(token) {
+    const { data: res } = await axios.get(`${process.env.REACT_APP_API_URL}/posts`, createConfig(token));
     return res;
 }
 
-async function toggleLike(idPost,token){
+async function getPostsAfterDate(date, token) {
+    const { data: res } = await axios.get(`${process.env.REACT_APP_API_URL}/posts/since/${date}`, createConfig(token));
+    return res;
+}
+
+async function toggleLike(idPost, token) {
     const body = {};
     return await axios.post(`${process.env.REACT_APP_API_URL}/posts/${idPost}/like`, body, createConfig(token));
 }
-async function updatePost(idPost, postDescription, token){
-    const body = {postDescription};
+async function updatePost(idPost, postDescription, token) {
+    const body = { postDescription };
     return await axios.put(`${process.env.REACT_APP_API_URL}/posts/${idPost}`, body, createConfig(token));
 }
 
-async function deletePost(idPost, token){
+async function deletePost(idPost, token) {
     return await axios.delete(`${process.env.REACT_APP_API_URL}/posts/${idPost}`, createConfig(token));
 }
 
-async function getPostsByUser(idUser, token){
-    const {data: res} = await axios.get(`${process.env.REACT_APP_API_URL}/user/${idUser}`,createConfig(token)) 
+async function getPostsByUser(idUser, token) {
+    const { data: res } = await axios.get(`${process.env.REACT_APP_API_URL}/user/${idUser}`, createConfig(token))
     return res
 }
 
-async function insertPost(description, link, token){
-    const body = {description, link}
+async function insertPost(description, link, token) {
+    const body = { description, link }
     return await axios.post(`${process.env.REACT_APP_API_URL}/create-post`, body, createConfig(token))
 }
 
@@ -41,6 +46,7 @@ const apiPosts = {
     getPosts,
     toggleLike,
     getPostsByUser,
-    insertPost
+    insertPost,
+    getPostsAfterDate
 }
 export default apiPosts;
