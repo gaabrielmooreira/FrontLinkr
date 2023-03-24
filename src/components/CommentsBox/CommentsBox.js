5ad42060fa085a86f5ee39de246f4e730c3cdda8
christaine <christaine.miranda@gmail.com>
2023-03-24 17:20:05 -0300

    fix: reload comments

--- src/components/CommentsBox/CommentsBox.js
+++ src/components/CommentsBox/CommentsBox.js
@@ -1,51 +1,56 @@
 import styled from "styled-components";
-import{BsSend} from "react-icons/bs";
-import { useEffect, useState ,useContext} from "react";
+import { BsSend } from "react-icons/bs";
+import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../context/auth";
 import CommentItem from "./CommentItem";
 import apiComments from "../../services/apiComments";
 
 
-export default function CommentsBox({showComments, setNumber, photo}) {
+export default function CommentsBox({ post, showComments, setNumber, photo }) {
     const { userAuth } = useContext(AuthContext);
 
     const [inputText, setInputText] = useState("");
     const [allComments, setAllComments] = useState([]);
+    const [reload, setReload] = useState(0);
 
     useEffect(() => {
-        try {
-            const commentsArray = apiComments.getAllComments(userAuth.token);
-            setAllComments([...commentsArray]);
-            setNumber(commentsArray.length);
-        
-        } catch (error) {
-            alert(error.message);
-        }
-    }, []);
+        async function loadComments() {
+            try {
+                const commentsArray = await apiComments.getAllComments(post, userAuth.token);
+                setAllComments([...commentsArray]);
+                setNumber(commentsArray.length);
 
-    async function sendComment(){
-        if(!inputText.length){
-           return alert("Não é possível fazer comentários vazios");
+            } catch (error) {
+                alert(error.message);
+            }
         }
-       
+        loadComments();
+    }, [reload]);
+
+    async function sendComment() {
+        if (!inputText.length) {
+            return alert("Não é possível fazer comentários vazios");
+        }
         try {
-            await apiComments.insertComment({comment: inputText}, userAuth.token );
+            const newId = await apiComments.insertComment(post, inputText, userAuth.token);
+            console.log(newId)
+            setReload(newId);
             setInputText("");
         } catch (error) {
             alert(error.message);
         }
-        
+
     }
 
     return (
         <Container showComments={showComments}>
             <ShowCommentsBox >
-                {allComments.length !== 0 ? allComments.map((item) => <CommentItem item={item} />) : ""}
+                {allComments.length !== 0 ? allComments.map((item) => <CommentItem  item={item} />) : ""}
             </ShowCommentsBox>
             <MakeComment>
-                <img src={photo}/>
-                <input type="text" placeholder="write a comment..." value={inputText} onChange={(event) => setInputText(event.target.value)}/>
-                <SendIcon onClick={sendComment}/>                
+                <img src={photo} />
+                <input type="text" placeholder="write a comment..." value={inputText} onChange={(event) => setInputText(event.target.value)} />
+                <SendIcon onClick={sendComment} />
             </MakeComment>
         </Container>
     )
--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -159,7 +159,7 @@ export default function PostCard({ post, postsAreChanged, setPostsAreChanged, is
                 </LinkContainer>
             </RightContainer>
         </Post>
-        <CommentsBox showComments={showComments} setNumber={setNumberComments} photo={photo_author}/>
+        <CommentsBox post={post_id} showComments={showComments} setNumber={setNumberComments} photo={photo_author}/>
         </ContainerGlobal>
     )
 }
